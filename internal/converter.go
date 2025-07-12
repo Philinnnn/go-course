ee8aa68b75176d4e59ce4182f9f673514c186d21
Philinnnn <armanela3157@gmail.com>
2025-07-12 22:18:41 +0500

    converter init

---
+++ internal/converter.go
@@ -0,0 +1,44 @@
+package internal
+
+import (
+	"errors"
+
+	"github.com/shopspring/decimal"
+)
+
+var ErrUnsupportedCurrency = errors.New("unsupported currency")
+
+const (
+	USD_KZT_rate = 522.37
+	EUR_KZT_rate = 610.93
+	RUB_KZT_rate = 6.70
+)
+
+func getKZTRate(currency string) (decimal.Decimal, error) {
+	switch currency {
+	case "USD":
+		return decimal.NewFromFloat(USD_KZT_rate), nil
+	case "EUR":
+		return decimal.NewFromFloat(EUR_KZT_rate), nil
+	case "RUB":
+		return decimal.NewFromFloat(RUB_KZT_rate), nil
+	default:
+		return decimal.Zero, ErrUnsupportedCurrency
+	}
+}
+
+func ConvertKZTTo(money decimal.Decimal, currency string) (decimal.Decimal, error) {
+	rate, err := getKZTRate(currency)
+	if err != nil {
+		return decimal.Zero, err
+	}
+	return money.Div(rate).Round(2), nil
+}
+
+func ConvertToKZT(money decimal.Decimal, currency string) (decimal.Decimal, error) {
+	rate, err := getKZTRate(currency)
+	if err != nil {
+		return decimal.Zero, err
+	}
+	return money.Mul(rate).Round(2), nil
+}
